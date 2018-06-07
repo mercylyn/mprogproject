198bba595ce78a03a3cb4ce37ea89267ab92e5e9
mercylyn <m.d.wiemer@gmail.com>
2018-06-07 14:54:25 +0200

    convert data Chart

--- mprog_project.js
+++ mprog_project.js
@@ -2,51 +2,48 @@
  * Name: Mercylyn Wiemer (10749306)
  * Course: programmeerproject
 
+http://bl.ocks.org/enjalot/1525346
+http://bl.ocks.org/mapsam/6090056
  **/
 
-// window.onload = function() {
-//
-//     const test = ""
-//
-//     d3.queue()
-//         .defer(d3.request, test)
-//         awaitAll(parseData);
-//
-//     function parseData(error, response) {
-//     if (error) throw error;
-//
-//     console.log(respons)
-//     }
-// }
+window.onload = function() {
 
-// d3.csv("beatles_chart_albums.csv", function(error, data) {
-//     if (error) throw error;
-//
-//     console.log(data);
-// })
+    queue()
+        .defer(d3.csv, "data/beatles_chart_albums.csv")
+        .defer(d3.csv, "data/beatles_chart_singles.csv")
+        .await(convertData);
 
-var albums_csv = function() {
-    var parse = d3.time.format("%m/%d/%Y").parse;
-    d3.csv("beatles_chart_albums.csv", function(album_chart) {
+    function convertData(error, albums, singles) {
+        if (error) throw error;
 
-        console.log(album_chart)
-        data = album_chart.map(function(d) {
-            console.log("d", d)
-            // date = parse(d.date);
-            // highestPosition = +d.highestPosition;
-            // weeksChart = +d.weeksChart;
-            // award = d.award;
-            // title = d.title;
-            // labelCatNo = d.labelCatNo;
-            // weeksNo1 = +d.weeksNo1;
-            // usNo1 = d.usNo1;
+        var parse = d3.time.format("%m/%d/%Y").parse;
+        var dataAlbums = albums.map(function(d) {
             return {"date" : parse(d.date), "highestPosition" : +d.highestPosition,
                     "weeksChart" : +d.weeksChart, "award" : d.award, "title" : d.title,
                     "labelCatNo" : d.labelCatNo, "weeksNo1" : +d.weeksNo1, "usNo1" : +d.usNo1};
+        });
+
+        var dataSingles = singles.map(function(d) {
+            return {"date" : parse(d.date), "highestPosition" : +d.highestPosition,
+                    "weeksChart" : +d.weeksChart, "title" : d.title,
+                    "artists" : d.artists, "weeksNo1" : +d.weeksNo1, "usNo1" : +d.usNo1};
         })
 
-        console.log("data", data)
-    })
+        var dataChart = {"Albums" : dataAlbums, "Singles" : dataSingles};
+
+        console.log("data chart", dataChart);
+
+        makeBarChart(dataChart);
+    }
 }
 
-albums_csv()
+makeBarChart(data) {
+    // http://bl.ocks.org/mstanaland/6100713
+    // Set dimensions of canvas
+    const margin = {top: 100, bottom: 50, right:30, left: 60},
+        width = 470 - margin.left - margin.right,
+        height = 470 - margin.top - margin.bottom,
+        barHeight = 20,
+        originChart = 0;
+
+}
--- test/aantekeningen_mprog_project.js
+++ test/aantekeningen_mprog_project.js
@@ -27,3 +27,44 @@ data = album_chart.map(function(d) {
             "weeksChart" : weeksChart, "award" : award, "title" : title,
             "labelCatNo" : labelCatNo, "weeksNo1" : weeksNo1, "usNo1" : usNo1};
 })
+
+// d3.csv("beatles_chart_albums.csv", function(error, data) {
+//     if (error) throw error;
+//
+//     console.log(data);
+// })
+
+// var dataAlbums, dataSingles;
+//
+// var chartCsv = function() {
+//     var parse = d3.time.format("%m/%d/%Y").parse;
+//     d3.csv("beatles_chart_albums.csv", function(albumsChart) {
+//
+//         dataAlbums = albumsChart.map(function(d) {
+//             return {"date" : parse(d.date), "highestPosition" : +d.highestPosition,
+//                     "weeksChart" : +d.weeksChart, "award" : d.award, "title" : d.title,
+//                     "labelCatNo" : d.labelCatNo, "weeksNo1" : +d.weeksNo1, "usNo1" : +d.usNo1};
+//         })
+//
+//         // console.log("data", dataAlbums)
+//     })
+//
+//     d3.csv("beatles_chart_singles.csv", function(singlesChart) {
+//
+//         dataSingles = singlesChart.map(function(d) {
+//             return {"date" : parse(d.date), "highestPosition" : +d.highestPosition,
+//                     "weeksChart" : +d.weeksChart, "title" : d.title,
+//                     "artists" : d.artists, "weeksNo1" : +d.weeksNo1, "usNo1" : +d.usNo1};
+//         })
+//
+//         // console.log("data", dataSingles)
+//     })
+//
+//     console.log(dataSingles)
+//     dataChart = {"Albums" : dataAlbums, "Singles" : dataSingles}
+//
+// }
+//
+// chartCsv()
+//
+// console.log(dataChart)
